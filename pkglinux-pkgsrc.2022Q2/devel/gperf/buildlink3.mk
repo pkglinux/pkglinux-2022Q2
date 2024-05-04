7bbec9d8382f8e04aa998bb0cc6a02aead91f1d1
pkglinux <jianjun1@126.com>
2024-05-04 13:43:49 -0400

    Update on 2024-05-04

--- pkglinux-pkgsrc.2022Q2/devel/gperf/buildlink3.mk
+++ pkglinux-pkgsrc.2022Q2/devel/gperf/buildlink3.mk
@@ -1,13 +1,22 @@
-# $NetBSD: buildlink3.mk,v 1.9 2009/03/20 19:24:15 joerg Exp $
+# $NetBSD$
+# XXX
+# XXX This file was created automatically using createbuildlink-3.18.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified buildlink3.mk files.
+# XXX
+# XXX Packages that only install static libraries or headers should
+# XXX include the following line:
+# XXX
+# XXX	BUILDLINK_DEPMETHOD.gperf?=	build
 
 BUILDLINK_TREE+=	gperf
 
 .if !defined(GPERF_BUILDLINK3_MK)
 GPERF_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gperf+=	gperf>=3.0.1
+BUILDLINK_API_DEPENDS.gperf+=	gperf>=3.1
 BUILDLINK_PKGSRCDIR.gperf?=	../../devel/gperf
-BUILDLINK_DEPMETHOD.gperf?=	build
-.endif # GPERF_BUILDLINK3_MK
+.endif	# GPERF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gperf
