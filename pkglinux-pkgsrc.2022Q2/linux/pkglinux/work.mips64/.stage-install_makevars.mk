9df0665bf3bc0e2c1f701ffb2a58d088172c1cf0
pkglinux <jianjun1@126.com>
2024-05-24 00:27:46 -0400

    Update on 2024-05-24

---
+++ pkglinux-pkgsrc.2022Q2/linux/pkglinux/work.mips64/.stage-install_makevars.mk
@@ -0,0 +1,13 @@
+.if !defined(_MAKEVARS_MK)
+_MAKEVARS_MK=	defined
+
+TOOLS_DEPENDS.ghostscript=	ghostscript>=6.01:../../print/ghostscript
+_BLNK_PHYSICAL_PATH.LOCALBASE=	/2022Q2-mips64abi64
+_BLNK_PHYSICAL_PATH.WRKDIR=	/sda3/jails/current/pkglinux-pkgsrc.2022Q2/linux/pkglinux/work.mips64
+_IGNORE_INFO_PATH=	
+_MANCOMPRESSED=	no
+_MANZ=	no
+_USE_TOOLS=	[ awk basename cat chgrp chmod chown cmp cp cut date diff dirname echo egrep env expr false fgrep find grep head hostname id install ln ls mkdir mv printf pwd rm rmdir sed sh sort tail test touch tr true uname wc xargs
+_WRAP_PATH=	/sda3/jails/current/pkglinux-pkgsrc.2022Q2/linux/pkglinux/work.mips64/.buildlink/bin:/sda3/jails/current/pkglinux-pkgsrc.2022Q2/linux/pkglinux/work.mips64/.tools/bin:/2022Q2-mips64abi64/bin:/2022Q2-mips64abi64/sbin:/2022Q2-mips64abi64/bin:/pkg/qt5/bin:/bin:/usr/bin:/sbin:/usr/sbin:/pkg/sbin:/pkg/bin:/sda4/sda3/go/bin
+
+.endif # _MAKEVARS_MK
