aa740a4d1236384f71adba9fb7b6f938349227f3
pkglinux <jianjun1@126.com>
2025-01-12 15:49:55 -0500

    Update on 2025-01-12

---
+++ pkglinux-pkgsrc.2022Q2/linux/rig/patches/patch-rig.cc
@@ -0,0 +1,12 @@
+$NetBSD$
+
+--- rig.cc.orig	2006-04-23 14:42:22.000000000 +0000
++++ rig.cc
+@@ -16,6 +16,7 @@
+  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+  * USA. */
+ 
++#include <cstring>
+ #include <iostream>
+ #include <iomanip>
+ #include <fstream>
