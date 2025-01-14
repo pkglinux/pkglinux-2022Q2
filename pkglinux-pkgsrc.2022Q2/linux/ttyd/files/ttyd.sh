8dfd3d29c7afcd3e72fcf669f14b872c757214f9
pkglinux <jianjun1@126.com>
2025-01-14 14:34:47 -0500

    Update on 2025-01-14

---
+++ pkglinux-pkgsrc.2022Q2/linux/ttyd/files/ttyd.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# $NetBSD: ttyd.sh,v 2.31 2025/01/07 11:53:29 Exp $
+#
+# PROVIDE: ttyd
+# REQUIRE: LOGIN
+
+. /etc/rc.subr
+
+name="ttyd"
+rcvar=$name
+command="@PREFIX@/bin/${name}"
+pidfile="/run/${name}.pid"
+
+if [ -f /etc/rc.subr -a -f /etc/rc.conf -a -f /etc/rc.d/DAEMON ]
+then
+	case ${1:-start} in
+	start)
+		if [ -e ${pidfile} ]; then
+			echo -n "${name} is already running at "
+			cat ${pidfile}
+		elif [ -x ${command} ];then
+			echo "Starting ${name}."
+			nohup $command --writable /bin/login &
+			echo $! > "$pidfile"
+			echo -n "${name} started at "
+			cat ${pidfile}
+		fi
+		;;
+	stop)
+		if [ -f ${pidfile} ]; then
+			pid=`/bin/head -1 ${pidfile}`
+			echo "Stopping ${name}."
+			kill -TERM ${pid}
+			rm -v ${pidfile}
+		else
+			echo "${name} not running?"
+		fi
+		;;
+	restart)
+		( $0 stop )
+		sleep 1
+		$0 start
+		;;
+	status)
+		if [ -f ${pidfile} ]; then
+			pid=`/bin/head -1 ${pidfile}`
+			echo "${name} is running as pid ${pid}."
+		else
+			echo "${name} is not running."
+		fi
+		;;
+	esac
+fi
+
